c73145f0da953995d3f80b64c6df671b17a32328
gac1989 <germanarena12@gmail.com>
2021-08-15 22:14:37 -0300

    Practico 1

---
+++ Practico1/Practico1/Program.cs
@@ -0,0 +1,119 @@
+﻿using Shared;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ManejadorPersonas manejador = new ManejadorPersonas();
+            string nombre = "";
+            string documento = "";
+            string fNac="";
+            string opcion = "";
+            List<string> personas = null;
+            
+            do
+            {
+                mostrarMenu();
+                opcion = Console.ReadLine();
+
+                switch (opcion)
+                {
+                    case "1":
+                        personas = manejador.listarPersonas().Select(x=>x.toString()).ToList();
+                        if (personas.Count > 0 )
+                        {
+                            Console.Clear();
+                            personas.ForEach(x =>
+                                {
+                                    Console.WriteLine(x);
+                                });
+                        }
+                        else
+                        {
+                            Console.Clear();
+                            Console.WriteLine("No hay personas ingresadas");
+                        }
+                        break;
+                    case "2":
+                        try
+                        { 
+                            Console.WriteLine("Ingresa tu nombre");
+                            nombre = Console.ReadLine();
+                            Console.WriteLine("Ingresa tu documento");
+                            documento = Console.ReadLine();
+                            Console.WriteLine("Ingresa tu fecha de nacimiento (dd-mm-aaaa)");
+                            fNac = Console.ReadLine();
+                            manejador.ingresarPersona(nombre, documento, fNac);
+                            Console.Clear();
+                         }
+                        catch(Exception e)
+                        {   
+                            Console.Clear();
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    case "3":
+                        try
+                        { 
+                            Console.WriteLine("Ingresa tu nombre");
+                            nombre = Console.ReadLine();
+                            Console.WriteLine("Ingresa tu documento");
+                            documento = Console.ReadLine();
+                            personas = manejador.filtrarPersonas(nombre, documento).Select(x => x.toString()).ToList();
+                            if (personas.Count > 0)
+                            {
+                                Console.Clear();
+                                personas.ForEach(x =>
+                                {
+                                    Console.WriteLine(x);
+                                });
+                            }
+                            else
+                            {
+                                Console.Clear();
+                                Console.WriteLine("No hay personas ingresadas con esos datos");
+                            }
+                        }
+                        catch(Exception e)
+                        {   
+                            Console.Clear();
+                            Console.WriteLine(e.Message);
+                        }
+                        break;
+                    case "4":
+                        break;
+                    default:
+                        Console.WriteLine("Ingresa una opcion valida");
+                        break;
+
+                }
+
+            } while (opcion != "4");
+
+            
+
+        }
+
+        public static void mostrarMenu()
+        {
+            Console.WriteLine("********MENU********");
+            Console.WriteLine("-------------------");
+            Console.WriteLine("1- Listar Personas");
+            Console.WriteLine("2- Ingresar Persona");
+            Console.WriteLine("3- Filtrar y Listar");
+            Console.WriteLine("4- Salir");
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Seleccione una opcion");
+        }
+
+    }
+}
---
+++ Practico1/Shared/ManejadorPersonas.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class ManejadorPersonas
+    {
+        public List<Persona> lista = new List<Persona>();
+
+        public List<Persona> listarPersonas()
+        {
+            return lista.OrderByDescending(x => x.getEdad()).ToList();
+        }
+
+        public void ingresarPersona(string nombre, string documento, string fechaNac)
+        {
+            if(nombre!=null && documento != null && fechaNac != null && nombre != "" && documento != "" && fechaNac != "")
+            {
+                DateTime nacimiento = DateTime.ParseExact(fechaNac, "dd-MM-yyyy", CultureInfo.InvariantCulture);
+                Persona p = new Persona(nombre, documento, nacimiento);
+                lista.Add(p);
+            }
+            else
+            {
+                throw new Exception ("ERROR: Datos incorrectos.");
+            }
+
+        }
+
+        public List<Persona> filtrarPersonas(string nombre, string documento)
+        {
+            if (nombre != "" || documento != "")
+            {    
+
+                    List<Persona> resultado = lista.Where(x => x.Nombre.Contains(nombre) && x.Documento.Contains(documento)).ToList(); //
+                    
+
+                return resultado;
+            }
+            else
+            {
+               throw new Exception ("No se ha encontrado ninguna persona.");
+            }
+        }
+    }
+}
